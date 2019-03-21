d91062e31699dbe133978ac37a3927c245b3fe1c
Jai Purandare <jai2005@gmail.com>
2019-03-20 18:53:14 -0700

    Added db initilization and app initialization

--- db.py
+++ db.py
@@ -18,3 +18,19 @@ def close_db():
 
 	if db in not None:
 		db.close()
+
+def init_db():
+	db = get_db()
+
+	with current_app.open_resource('schema.sql') as f:
+		db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+	init_db()
+	click.echo('Initialized database.')
+
+def init_app(app):
+	app.teardown_appcontext(close_db)
+	app.cli.add_command(init_db_command)
