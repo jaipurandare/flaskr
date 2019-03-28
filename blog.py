ff8d9ca10c2c5afe2847b066ecd845f48ad2f7c9
Jai Purandare <jai2005@gmail.com>
2019-03-28 16:00:36 -0700

    Added capability to delete post

--- blog.py
+++ blog.py
@@ -52,6 +52,15 @@ def update(id):
 			return redirect(url_for('blog.index'))
 	return render_template('blog/update.html', post=post)
 
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+	get_post(id)
+	db = get_db()
+	db.execute('DELETE FROM post WHERE id = ?', (id,))
+	db.commit()
+	return redirect(url_for('blog.index'))
+
 def get_post(id, check_author=True):
 	post = get_db().execute('''SELECT p.id, title, body, created, author_id, username 
 								FROM post p JOIN user u ON p.author_id=u.id 
