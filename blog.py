8027bc8abae237cd208c6e053f53b4e5dbe72561
Jai Purandare <jai2005@gmail.com>
2019-03-28 15:52:37 -0700

    Added capability to update blog

--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
 from flask import ( Blueprint, flash, g, redirect, render_template, request, url_for )
 
+from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
@@ -34,6 +35,32 @@ def create():
 
 	return render_template('blog/create.html')
 
-@bp.route('/update')
-def update():
-	return render_template('blog/update.html')
+@bp.route('/<int:id>/update', methods=('GET','POST',))
+@login_required
+def update(id):
+	post = get_post(id)
+
+	if request.method == 'POST':
+		title = request.form['title']
+		if title is None:
+			flash('Title is required.')
+		else:
+			body = request.form['body']
+			db = get_db()
+			db.execute('UPDATE post SET title = ?, body = ? WHERE id = ?', (title, body, id))
+			db.commit()
+			return redirect(url_for('blog.index'))
+	return render_template('blog/update.html', post=post)
+
+def get_post(id, check_author=True):
+	post = get_db().execute('''SELECT p.id, title, body, created, author_id, username 
+								FROM post p JOIN user u ON p.author_id=u.id 
+								WHERE p.id = ?''', (id,)).fetchone()
+	if post is None:
+		abort(404, "Post id {0} does not exist.".format(id))
+
+	if check_author and post['author_id'] != g.user['id']:
+		abort(403)
+
+	return post
+
